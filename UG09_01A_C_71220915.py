852ced512af4b0547192a7115ac8488113560fdc
ZeroAce25 <117893657+ZeroAce25@users.noreply.github.com>
2022-11-17 16:25:46 +0700

    Add files via upload

---
+++ UG09_01A_C_71220915.py
@@ -0,0 +1,7 @@
+#print("helo world")
+#case 1
+Nama = input("Masukkan nama anda :")
+Kuliah = input("Masukkan mata kuliah :")
+Grup = input("Masukkan grup :")
+print("haloo! ",Nama[0:10])
+print(Nama[0:10],"tergabung dalam kelas",Kuliah,"pada",Grup)
---
+++ UG09_01B_C_71220915.py
@@ -0,0 +1,7 @@
+#print("helo world")
+#case 2
+Nama = input("Masukkan nama anda :")
+Kuliah = input("Masukkan mata kuliah :")
+Grup = input("Masukkan grup :")
+print("haloo! ",Nama)
+print(Nama[0:7],"tergabung dalam kelas",Kuliah,"pada",Grup)
